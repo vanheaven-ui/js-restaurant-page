64947c09be845079effabdf0119d0b9e7f8e9a18
vanheaven <vanheaven6@gmail.com>
2020-12-13 10:49:01 +0300

    Add nav module

---
+++ src/modules/nav.js
@@ -0,0 +1,22 @@
+import helperModule from './helpers.js';
+
+const navItems = `
+  <div>
+    UDine
+    signature cuisines await you...
+  </div>
+  <div>
+    <button class="nav-button active">HOME</button>
+    <button class="nav-button">MENU</button>
+    <button class="nav-button">CONTACT</button>
+  </div>
+  `;
+
+  const header = () => {
+    const hdr = helperModule.createTag('nav', navItems);
+    hdr.setAttribute('id', 'nav-bar');
+    helperModule.appendTag(helperModule.wrapperDiv(), hdr);
+  };
+
+  export default header;
+  
